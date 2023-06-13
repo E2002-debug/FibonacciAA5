8fe3803fb2c4fc6ead6663b8b0fa5a313eb3f75f
Estefania2002 <84207188+E2002-debug@users.noreply.github.com>
2023-06-12 20:25:53 -0500

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelofibonacci;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @Estefania Cale
+ */
+public class Fibonacci {
+
+    private HashMap<Integer, Long> fibonacciMap;
+
+    public Fibonacci() {
+        fibonacciMap = new HashMap<>();
+        fibonacciMap.put(0, 0L);
+        fibonacciMap.put(1, 1L);
+    }
+//metodo recursivo
+
+    public long getFibonacciNumber(int n) {
+        if (fibonacciMap.containsKey(n)) {
+            return fibonacciMap.get(n);
+        } else {
+            long fibValue = getFibonacciNumber(n - 1) + getFibonacciNumber(n - 2);
+            fibonacciMap.put(n, fibValue);
+            return fibValue;
+        }
+
+    }
+
+    public int getFibonacciPosition(long num) {
+        if (num == 1) {
+            return 1;
+        }
+
+        if (num == 1) {
+            return 1;
+        }
+
+        int position = 2;
+        long fibMinusTwo = 0;
+        long fibMinusOne = 1;
+        long fibValue = fibMinusOne + fibMinusTwo;
+
+        while (fibValue < num) {
+            fibMinusTwo = fibMinusOne;
+            fibMinusOne = fibValue;
+            fibValue = fibMinusOne + fibMinusTwo;
+            position++;
+        }
+
+        if (fibValue == num) {
+            return position;
+        } else {
+            return -1; // El número no está en la serie de Fibonacci si es menor a -1
+        }
+    }
+
+    public List<Long> getFibonacciSequence(int n) {
+        List<Long> sequence = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            long fib = getFibonacciNumber(i);
+            sequence.add(fib);
+        }
+        return sequence;
+    }
+
+}
---
+++ ModeloFibonacci.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modelofibonacci;
+
+
+
+/**
+ *
+ * @author Usuario_PC
+ */
+public class ModeloFibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("=====GRUPO 4======");
+        System.out.println("Erika Curimilma, Kerly Huachaca, Mariuxi Valdez, Estefania Cale");
+         Menu menu = new Menu();
+        menu.run();
+         
+    }
+}
